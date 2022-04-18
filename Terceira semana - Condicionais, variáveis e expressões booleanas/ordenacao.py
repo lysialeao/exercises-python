044cbca843abcf2d4c0fcedb07d1fcab85efc104
Lysia Leão <lysialeao@residuall.org>
2022-04-18 17:36:07 -0300

    Solving conflicts

--- Terceira semana - Condicionais, variáveis e expressões booleanas/ordenacao.py
+++ Terceira semana - Condicionais, variáveis e expressões booleanas/ordenacao.py
@@ -1,7 +1,14 @@
+# Receba 3 números inteiros na entrada e imprima
+
+# crescente
+
+# se eles forem dados em ordem crescente. Caso contrário, imprima 
+
+# não está em ordem crescente
+
 numero1 = int(input("Digite o 1º número: "))
 numero2 = int(input("Digite o 2º número: "))
 numero3 = int(input("Digite o 3º número: "))
-
 
 if(numero1<numero2 and numero2<numero3):
     print('crescente')
