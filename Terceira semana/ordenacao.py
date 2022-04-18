f39638e870dda1a3dcb72dc06509aba5f34a56a8
Lysia Leão <lysialeao@residuall.org>
2022-04-18 17:22:40 -0300

    First commit

---
+++ Terceira semana/ordenacao.py
@@ -0,0 +1,9 @@
+numero1 = int(input("Digite o 1º número: "))
+numero2 = int(input("Digite o 2º número: "))
+numero3 = int(input("Digite o 3º número: "))
+
+
+if(numero1<numero2 and numero2<numero3):
+    print('crescente')
+else :
+    print('não está em ordem crescente')
---
+++ primeiro_exercicio.py
@@ -0,0 +1,5 @@
+temperatura = input("Digite uma temperatura em Fahrenheit: ")
+
+temperaturaCelsius = (int(temperatura) - 32) * 5 // 9
+
+print("A temperatura em Celsius é: ",temperaturaCelsius);
