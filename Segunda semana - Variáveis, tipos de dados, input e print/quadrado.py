72bf7010e7690d6e8570f379d7d254514a7cc88b
Lysia Leão <lysialeao@residuall.org>
2022-04-18 17:32:17 -0300

    Adding comments that explain the exercises

---
+++ Segunda semana - Variáveis, tipos de dados, input e print/quadrado.py
@@ -0,0 +1,10 @@
+from unittest import result
+
+
+ladoQuad = int(input("Digite o valor correspondente ao lado de um quadrado: "))
+
+perimetro = str(ladoQuad * 4)
+area = str(ladoQuad*ladoQuad)
+
+print("perímetro: " + perimetro + " - área: " + area)
+
--- Terceira semana/paridade.py
+++ Terceira semana/paridade.py
@@ -1,3 +1,13 @@
+# Receba um número inteiro na entrada e imprima
+
+#  par 
+
+# quando o número for par ou
+
+# ímpar
+
+# quando o número for ímpar.
+
 numero = int(input("Digite um número: "))
 
 if(numero%2==0) :
